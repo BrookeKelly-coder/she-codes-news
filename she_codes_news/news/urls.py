495e440f644e907fcd8859bc1da4b1c8e6ae46b1
BrookeKelly-coder <86648602+BrookeKelly-coder@users.noreply.github.com>
2021-08-24 17:31:49 +1000

    Django Sessions Forms + User Apps

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'news'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.StoryView.as_view(), name='story'),
-    path('add-story/', views.AddStoryView.as_view(), name='newStory') 
+    path('', views.IndexView.as_view(), name='index'), #/news/
+    path('<int:pk>/', views.StoryView.as_view(), name='story'), #/news/3 story number 3
+    path('add-story/', views.AddStoryView.as_view(), name='newStory') #/news/add-story form
 ]
